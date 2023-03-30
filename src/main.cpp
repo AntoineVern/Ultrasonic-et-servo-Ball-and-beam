428690ec3116225776fb747e78108e599d77554d
ZazouSam <zazou2000@hotmail.ca>
2023-03-30 18:38:48 -0400

    Énorme changement sur le code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
-#include <HCSR04.h>
-#include <AccelStepper.h>
-#include <PID_v1.h>
-#include <A4988.h>
+#include <Arduino.h>
+#include "A4988.h"
 
-UltraSonicDistanceSensor distanceSensor(A0, A1); // Initialize sensor that uses digital pins 13 and 12.
-
-#define PIN_INPUT A1
-#define PIN_OUTPUT 32
-
-// Define Variables we'll be connecting to
-double Setpoint, Input, Output;
-
-// Specify the links and initial tuning parameters
-double Kp = 0.5, Ki = 0.0, Kd = 0.0;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-
-///////////////////////Inputs/outputs///////////////////////
+// using a 200-step motor (most common)
+#define MOTOR_STEPS 200
+// configure the pins connected///////////////////////Inputs/outputs///////////////////////
 // Define the pin connections for the A4988 driver
 #define DIR_PIN 14
 #define STEP_PIN 32
@@ -23,157 +11,104 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 #define MS2_PIN 12
 #define MS3_PIN 27
 
-#define MAX 40
-#define MIN 0
-// Define the number of steps per revolution of the stepper motor
-int microsteps = 1;
-const int stepsPerRevolution = 200 * microsteps;
+#define MAX 45
+#define MIN -45
 
 #define motorInterfaceType 1
-AccelStepper stepper(motorInterfaceType, STEP_PIN, DIR_PIN);
-int encAngle;
-///////////////////////////////////////////////////////
+A4988 stepper(MOTOR_STEPS, DIR_PIN, STEP_PIN, MS1_PIN, MS2_PIN, MS3_PIN);
+double angle_P, angle_I, angle_D, angle_PID, angle_now;
 
-////////////////////////Variables///////////////////////
-double distance;
-double lastDistance;
-int Read = 0;
-float elapsedTime, temps, timePrev; // Variables for time control
-float distance_previous_error, distance_error;
+#include <HCSR04.h>
+UltraSonicDistanceSensor distanceSensor(A0, A1); // Initialize sensor that uses digital pins 13 and 12.
+
+double distance = 0.0;
+double temps; // Variables for time control
+double distance_previous_error, distance_error;
 int period = 50; // Refresh rate period of the loop is 50ms
-int steps = 0;
+
+///////////////////PID constants///////////////////////
+double kp = 2;                 // Mine was 8
+double ki = 0.2;               // Mine was 0.2
+double kd = 3100;              // Mine was 3100
+double distance_setpoint = 20; // Should be the distance from sensor to the middle of the bar in mm
 ///////////////////////////////////////////////////////
 
+double limit(void);
+float measureDistance();
+void Kpot(void);
+
+void setup()
+{
+  Serial.begin(115200);
+  stepper.begin(60, 1); // vitesse est de 1.8°/s
+  temps = millis();
+  angle_now = 0;
+}
+
+void loop()
+{
+
+  if (millis() > temps + period)
+  {
+    temps = millis();
+    Kpot();
+    distance = measureDistance();
+    distance_error = distance_setpoint - distance;
+
+    angle_P = kp * distance_error;
+
+    float dist_diference = distance_error - distance_previous_error;
+    angle_D = kd * ((distance_error - distance_previous_error) / period);
+
+    if (-3 < distance_error && distance_error < 3)
+    {
+      angle_I = angle_I + (ki * distance_error);
+    }
+    else
+    {
+      angle_I = 0;
+    }
+
+    angle_PID = angle_P + angle_I + angle_D;
+    angle_PID = map(angle_PID, -150, 150, 0, 150);
+
+    if (angle_PID < 20)
+    {
+      angle_PID = 20;
+    }
+    if (angle_PID > 160)
+    {
+      angle_PID = 160;
+    }
+
+    stepper.rotate(limit());
+    distance_previous_error = distance_error;
+    printf("distance: %f error: %f angle: %f ", distance, distance_error, angle_P);
+    printf("kp: %f ki: %f kd: %f \n", kp, ki, kd);
+  }
+}
 float measureDistance()
 {
   return distanceSensor.measureDistanceCm();
 }
 
-void setMicrostep(int steps)
+double limit(void)
 {
-  if (steps == 1)
-  {
-    digitalWrite(MS1_PIN, LOW);
-    digitalWrite(MS2_PIN, LOW);
-    digitalWrite(MS3_PIN, LOW);
-  }
-  else if (steps == 2)
-  {
-    digitalWrite(MS1_PIN, HIGH);
-    digitalWrite(MS2_PIN, LOW);
-    digitalWrite(MS3_PIN, LOW);
-  }
-  else if (steps == 4)
-  {
-    digitalWrite(MS1_PIN, LOW);
-    digitalWrite(MS2_PIN, HIGH);
-    digitalWrite(MS3_PIN, LOW);
-    microsteps = 4;
-  }
-  else if (steps == 8)
-  {
-    digitalWrite(MS1_PIN, HIGH);
-    digitalWrite(MS2_PIN, HIGH);
-    digitalWrite(MS3_PIN, LOW);
-    microsteps = 8;
-  }
-  else if (steps == 16)
-  {
-    digitalWrite(MS1_PIN, HIGH);
-    digitalWrite(MS2_PIN, HIGH);
-    digitalWrite(MS3_PIN, HIGH);
-  }
+  double target_angle = angle_now + angle_P;
+  target_angle = target_angle > MAX ? MAX : target_angle;
+  target_angle = target_angle < MIN ? MIN : target_angle;
+  double rotation = target_angle - angle_now;
+  angle_now = target_angle;
+  return rotation;
 }
 
-void safety(int steps)
+void Kpot(void)
 {
-  steps = steps > MAX ? MAX : steps;
-  steps = steps < MIN ? MIN : steps;
-}
+  uint16_t ADC_Value1 = analogRead(A2);
+  uint16_t ADC_Value2 = analogRead(A3);
+  uint16_t ADC_Value3 = analogRead(A4);
 
-void rotate(int steps, bool clockwise)
-{
-  // Set the direction of rotation
-  digitalWrite(DIR_PIN, clockwise ? HIGH : LOW);
-  safety(steps);
-  // Step the motor the specified number of steps
-  for (int i = 0; i < steps; i++)
-  {
-    digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(500);
-  }
-}
-
-void tourner_angle(double angle, bool clockwise)
-{
-  //deplacer(angle);
-  int steps = (angle * stepsPerRevolution) / 360;
-  rotate(steps, clockwise);
-}
-
-void setup()
-{
-  // Initialize the serial port
-  Serial.begin(115200);
-  Input = measureDistance();
-  Setpoint = 20;
-  // Set the A4988 driver pins as outputs
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(MS1_PIN, OUTPUT);
-  pinMode(MS2_PIN, OUTPUT);
-  pinMode(MS3_PIN, OUTPUT);
-  temps = millis();
-  setMicrostep(1);
-  // turn the PID on
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(-40, 40);
-  stepper.setMaxSpeed(1000);
-  stepper.setAcceleration(500);
-}
-
-void loop()
-{
-  // Set the target position for the stepper motor
-  int targetPosition = 100;
-
-  // Move the stepper motor to the target position
-  stepper.moveTo(targetPosition);
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();
-  }
-
-  // Delay for 1 second before moving to the next position
-  delay(1000);
-  // if (millis() > temps + period)
-  // {
-  //   if (Input < 0)
-  //   {
-  //     tourner_angle(Output, true);
-  //     steps++;
-  //   }
-  //   else
-  //   {
-  //     tourner_angle(Output, false);
-  //     steps--;
-  //   }
-  //   Serial.print("steps: ");
-  //   Serial.print(steps);
-  //   rotate(1, true);
-  //   Input = measureDistance();
-  //   Serial.print("  Input: ");
-  //   Serial.print(Input);
-  //   myPID.Compute();
-  //   // analogWrite(PIN_OUTPUT, Output);
-
-    
-  //   Serial.print(" Output: ");
-  //   Serial.print(Output);
-  //   Serial.print(" error: ");
-  //   Serial.println(Setpoint - Input);
-    
-    // myservo.write(PID_total+30);
-  // }
+  kp = 10.0 * ADC_Value1/ 4096.0;
+  ki = 1.0 * ADC_Value2/ 4096.0;
+  kd = 10000.0 * ADC_Value3/ 4096.0;
 }
